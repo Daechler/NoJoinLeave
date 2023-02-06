17e57717befdfbf5b54170a04761c9ee815515a0
Daechler <50748803+Daechler@users.noreply.github.com>
2023-02-06 11:52:20 +0100

    Add files via upload

---
+++ src/main/java/net/daechler/nojoinleave/NoJoinLeave.java
@@ -0,0 +1,24 @@
+package net.daechler.nojoinleave;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NoJoinLeave extends JavaPlugin implements Listener {
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        event.setJoinMessage(null);
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent event) {
+        event.setQuitMessage(null);
+    }
+}
